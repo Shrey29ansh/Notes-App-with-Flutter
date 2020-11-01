dd15305390cff03718f8d817e7577014f9453dd7
Shreyansh Sharma <47782066+Shrey29ansh@users.noreply.github.com>
2020-11-01 13:06:04 +0530

    UI Customization
    delete button added
    edit button added

--- chat_app/lib/locker.dart
+++ chat_app/lib/locker.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 // ignore: unused_import
 import 'package:chat_app/home.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter/services.dart';
 import 'package:chat_app/database_helper.dart';
 import 'package:local_auth/auth_strings.dart';
@@ -34,7 +35,7 @@ class _LockerState extends State<Locker> {
       this.username,
       this.color});
   final dbHelper = mainDB.instance, number = TextEditingController();
-
+  double _bottom = -60;
   bool nextscreen = false;
   bool show = false;
   Future _query() async {
@@ -129,6 +130,17 @@ class _LockerState extends State<Locker> {
     });
   }
 
+  Future startTimer2() async {
+    var _duration = new Duration(milliseconds: 300);
+    return new Timer(_duration, opaa2);
+  }
+
+  opaa2() async {
+    setState(() {
+      _bottom = 60;
+    });
+  }
+
   var exactval;
   void checklock() async {
     if (await _isBiometricAvailable()) {
@@ -138,6 +150,7 @@ class _LockerState extends State<Locker> {
         setState(() {
           show = true;
         });
+        startTimer2();
       }
     } else {
       setState(() {
@@ -159,180 +172,216 @@ class _LockerState extends State<Locker> {
     return Scaffold(
       body: Container(
         color: color,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            show
-                ? Center(
-                    child: Container(
-                      width: MediaQuery.of(context).size.width * .9,
-                      height: MediaQuery.of(context).size.width * .9,
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.center,
+        child: show
+            ? Stack(
+                children: [
+                  Align(
+                      alignment: Alignment.centerLeft,
+                      child: ClipRRect(
+                        child: Icon(
+                          Icons.lock_open_sharp,
+                          size: 800,
+                          color: Colors.amber,
+                        ),
+                      )),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
                         children: [
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Icon(
-                                Icons.lock_open,
-                                size: 50,
+                          Text(
+                            "$lockername",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
                                 color: Colors.white,
-                              ),
-                              Expanded(
-                                child: Text(
-                                  "Locker: $lockername",textAlign: TextAlign.center,                                  style: TextStyle(
-                                      color: Colors.white,
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 30),
-                                  overflow: TextOverflow.clip,
-                                  maxLines: 3,
-                                ),
-                              ),
-                            ],
+                                fontWeight: FontWeight.bold,
+                                fontSize: 30),
+                            overflow: TextOverflow.clip,
+                            maxLines: 3,
                           ),
-                          Card(
-                            elevation: 10,
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(10)),
-                            child: Center(
-                              child: Column(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceEvenly,
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Row(
-                                    children: [
-                                      SizedBox(
-                                        width: 40,
-                                      ),
-                                      Text(
-                                        "Username: $username\nPassword: $password\nComments: $comments",
-                                        style: TextStyle(
-                                            color: Colors.black,
-                                            fontWeight: FontWeight.bold,
-                                            fontSize: 15),
-                                      ),
-                                    ],
-                                  ),
-                                ],
-                              ),
+                          SizedBox(
+                            width: 7,
+                          ),
+                          Icon(
+                            Icons.vpn_key,
+                            size: 50,
+                            color: Colors.white,
+                          ),
+                        ],
+                      ),
+                      Container(
+                        height: MediaQuery.of(context).size.height * 0.2,
+                        width: MediaQuery.of(context).size.width * 0.9,
+                        child: Card(
+                          elevation: 10,
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10)),
+                          child: Center(
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Row(
+                                  children: [
+                                    SizedBox(
+                                      width: 40,
+                                    ),
+                                    Text(
+                                      "Username: $username\nPassword: $password\nComments: $comments",
+                                      style: TextStyle(
+                                          color: Colors.black,
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 17),
+                                    ),
+                                  ],
+                                ),
+                              ],
                             ),
                           ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  AnimatedPositioned(
+                    duration: Duration(milliseconds: 500),
+                    bottom: _bottom,
+                    right: MediaQuery.of(context).size.width * 0.25,
+                    curve: Curves.easeOutQuart,
+                    child: Container(
+                      margin: EdgeInsets.all(10),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          RaisedButton(
+                            color: color,
+                            child: Icon(
+                              Icons.delete,
+                              color: Colors.white,
+                            ),
+                            onPressed: null,
+                          ),
+                          SizedBox(
+                            width: 5,
+                          ),
+                          RaisedButton(
+                              child: Icon(
+                                Icons.edit,
+                                color: Colors.white,
+                              ),
+                              onPressed: null)
                         ],
                       ),
                     ),
                   )
-                : showcont
-                    ? Align(
-                        alignment: Alignment.center,
-                        child: AnimatedOpacity(
-                          duration: Duration(milliseconds: 500),
-                          opacity: _visible ? 1 : 0,
-                          child: Container(
-                            width: MediaQuery.of(context).size.width * 0.8,
-                            height: MediaQuery.of(context).size.height * 0.4,
-                            child: Card(
-                              elevation: 10,
-                              color: Colors.white,
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(10),
-                              ),
-                              child: Column(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceEvenly,
+                ],
+              )
+            : showcont
+                ? Align(
+                    alignment: Alignment.center,
+                    child: AnimatedOpacity(
+                      duration: Duration(milliseconds: 500),
+                      opacity: _visible ? 1 : 0,
+                      child: Container(
+                        width: MediaQuery.of(context).size.width * 0.8,
+                        height: MediaQuery.of(context).size.height * 0.4,
+                        child: Card(
+                          elevation: 10,
+                          color: Colors.white,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
                                 children: [
-                                  Row(
-                                    mainAxisAlignment: MainAxisAlignment.center,
+                                  Column(
                                     children: [
-                                      Column(
-                                        children: [
-                                          Text(
-                                            "Enter Pin",
-                                            style: TextStyle(
-                                                color: Colors.black,
-                                                fontWeight: FontWeight.bold,
-                                                fontSize: 20),
-                                          ),
-                                        ],
+                                      Text(
+                                        "Enter Pin",
+                                        style: TextStyle(
+                                            color: Colors.black,
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: 20),
                                       ),
                                     ],
                                   ),
-                                  Container(
-                                    width:
-                                        MediaQuery.of(context).size.width * 0.7,
-                                    child: Directionality(
-                                      textDirection: TextDirection.ltr,
-                                      child: TextField(
-                                        onTap: () {
-                                          setState(() {});
-                                        },
-                                        obscureText: true,
-                                        cursorColor: Colors.black,
-                                        controller: number,
-                                        autocorrect: true,
-                                        textAlign: TextAlign.center,
-                                        keyboardType: TextInputType.number,
-                                        decoration: InputDecoration(
-                                          focusColor: Colors.blue,
-                                          errorText: warn,
-                                          errorStyle: TextStyle(),
-                                          hintText: "Enter PIN",
-                                          border: OutlineInputBorder(
-                                            borderRadius:
-                                                BorderRadius.circular(10.0),
-                                            borderSide: BorderSide(
-                                              color: Colors.amber,
-                                              style: BorderStyle.solid,
-                                            ),
-                                          ),
+                                ],
+                              ),
+                              Container(
+                                width: MediaQuery.of(context).size.width * 0.7,
+                                child: Directionality(
+                                  textDirection: TextDirection.ltr,
+                                  child: TextField(
+                                    onTap: () {
+                                      setState(() {});
+                                    },
+                                    obscureText: true,
+                                    cursorColor: Colors.black,
+                                    controller: number,
+                                    autocorrect: true,
+                                    textAlign: TextAlign.center,
+                                    keyboardType: TextInputType.number,
+                                    decoration: InputDecoration(
+                                      focusColor: Colors.blue,
+                                      errorText: warn,
+                                      errorStyle: TextStyle(),
+                                      hintText: "Enter PIN",
+                                      border: OutlineInputBorder(
+                                        borderRadius:
+                                            BorderRadius.circular(10.0),
+                                        borderSide: BorderSide(
+                                          color: Colors.amber,
+                                          style: BorderStyle.solid,
                                         ),
                                       ),
                                     ),
                                   ),
-                                  RaisedButton(
-                                      shape: RoundedRectangleBorder(
-                                        borderRadius: BorderRadius.circular(5),
-                                      ),
-                                      elevation: 5,
-                                      child: Text(
-                                        'Submit',
-                                        style: TextStyle(
-                                          color: Colors.white,
-                                        ),
-                                      ),
-                                      color: Colors.blue,
-                                      onPressed: () async {
-                                        print(number.text);
-                                        if (number.text.isEmpty) {
-                                          setState(() {
-                                            warn = "Pin is required";
-                                          });
-                                        } else {
-                                          await _checkpin();
-                                          if (exactval == number.text) {
-                                            setState(() {
-                                              show = true;
-                                            });
-                                          } else {
-                                            setState(() {
-                                              warn = 'Try Again!';
-                                              wrong = true;
-                                            });
-                                          }
-                                        }
-
-                                        number.clear();
-                                      }),
-                                ],
+                                ),
                               ),
-                            ),
+                              RaisedButton(
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(5),
+                                  ),
+                                  elevation: 5,
+                                  child: Text(
+                                    'Submit',
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                  color: Colors.blue,
+                                  onPressed: () async {
+                                    print(number.text);
+                                    if (number.text.isEmpty) {
+                                      setState(() {
+                                        warn = "Pin is required";
+                                      });
+                                    } else {
+                                      await _checkpin();
+                                      if (exactval == number.text) {
+                                        setState(() {
+                                          show = true;
+                                        });
+                                      } else {
+                                        setState(() {
+                                          warn = 'Try Again!';
+                                          wrong = true;
+                                        });
+                                      }
+                                    }
+
+                                    number.clear();
+                                  }),
+                            ],
                           ),
                         ),
-                      )
-                    : Container(
-                        child: Center(child: CircularProgressIndicator())),
-          ],
-        ),
+                      ),
+                    ),
+                  )
+                : Container(child: Center(child: CircularProgressIndicator())),
       ),
     );
   }
